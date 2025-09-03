62e3f0ed765899acbd04d6ef25ab4002816553a7
jabsonDevElias <jabsonDevElias>
2025-09-03 09:49:14 -0300

    feat: Primeiro commit

---
+++ backend/database/migrations/2025_09_03_123946_create_musicas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('musicas', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->integer('visualizacoes');
+            $table->text('youtube_id');
+            $table->text('thumb');
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+
+            //             titulo TEXT NOT NULL,
+            // visualizacoes INTEGER NOT NULL,
+            // youtube_id TEXT NOT NULL,
+            // thumb TEXT NOT NULL,
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('musicas');
+    }
+};
